5f52e1d71f61b9a9f05122f1ee78f2383d7f99e3
sugasaki <sugasaki@gmail.com>
2021-08-08 15:12:22 +0900

    first commit

---
+++ src/main/kotlin/docker/ktor/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package docker.ktor.plugins
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!!!!!!")
+        }
+
+        get("/news") {
+            call.respondText("news!")
+        }
+    }
+}
